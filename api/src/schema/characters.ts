cb449dc3e9cd2df9c39d12c8a705b8b2858918fb
Marcin Komar <marcinkomar97@gmail.com>
2026-01-06 18:44:45 +0100

    feat: add characters GraphQL schema
    - Define Character type with all Rick & Morty API fields
    - Add Location type for origin and location
    - Add CharactersResponse with pagination support
    - Extend Query type with characters (paginated) and character (single)
    - Compose schema with auth and favoriteCharacters schemas

---
+++ api/src/schema/characters.ts
@@ -0,0 +1,38 @@
+export const charactersTypeDefs = `
+  type Character {
+    id: ID!
+    name: String!
+    status: String
+    species: String
+    type: String
+    gender: String
+    origin: Location
+    location: Location
+    image: String
+    episode: [String!]!
+    created: String
+  }
+
+  type Location {
+    name: String
+    url: String
+  }
+
+  type PageInfo {
+    count: Int!
+    pages: Int!
+    next: Int
+    prev: Int
+  }
+
+  type CharactersResponse {
+    results: [Character!]!
+    info: PageInfo!
+  }
+
+  extend type Query {
+    characters(page: Int): CharactersResponse!
+    character(id: ID!): Character
+  }
+`;
+
--- api/src/schema/index.ts
+++ api/src/schema/index.ts
@@ -1,8 +1,10 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { authTypeDefs } from "./auth.ts";
+import { charactersTypeDefs } from "./characters.ts";
+import { favoriteCharactersTypeDefs } from "./favoriteCharacters.ts";
 import { authResolvers } from "@/resolvers/auth.ts";
 
-const typeDefs = [authTypeDefs];
+const typeDefs = [authTypeDefs, charactersTypeDefs, favoriteCharactersTypeDefs];
 
 const resolvers = [authResolvers];
 
