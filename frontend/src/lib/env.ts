368f3bc1e00e1851e5c391bc4402c46f79cffd27
Marcin Komar <marcinkomar97@gmail.com>
2026-03-26 14:57:41 +0100

    test envs

--- frontend/src/lib/env.ts
+++ frontend/src/lib/env.ts
@@ -22,7 +22,7 @@ function getEnv() {
 
   return {
     SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-    SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_ANON_KEY ?? '',
+    SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
   };
 }
 
